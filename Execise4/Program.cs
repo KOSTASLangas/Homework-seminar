e61479a4240811bace3d0606c8e88028ad47c4f8
Kostas <langas.kostya@mail.ru>
2022-07-11 16:17:42 +0500

    нахождение макс из трех

---
+++ Execise4/Program.cs
@@ -0,0 +1,14 @@
+﻿int a,b,c;
+Console.WriteLine ("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите третье число");
+c = Convert.ToInt32(Console.ReadLine ());
+int max = a;
+if ((a > b) && (a > c)) max = a;
+if ((b > a) && (b > c)) max = b;
+if ((c > a) && (c > b)) max = c;
+
+Console.Write ("max = ");
+Console.Write (max);

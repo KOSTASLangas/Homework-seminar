dfff4fa59362a8cbe36c25e55684256265e24a31
Kostas <langas.kostya@mail.ru>
2022-07-11 16:39:59 +0500

    определение четности в пределах n

---
+++ Execise8/Program.cs
@@ -0,0 +1,16 @@
+﻿int n ;
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= n )
+{
+    if (i % 2 == 0)
+    {
+    Console.Write(i);
+    Console.Write(" ");
+    }
+    i++;
+
+}
